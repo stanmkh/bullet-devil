a6ffcd21b80515e69bd84b4ca9c170d2cdb17b6b
Stanislav Mikheyev <stanislav.mikheyev@gmail.com>
2025-04-12 00:54:35 +0200

    Move player projectile firing logic into player script, as this functionality does not appear to be reusable

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,21 +2,21 @@
 extends CharacterBody2D
 
 @export var speed = 500.0
-@export var fire_controller: Node2D
 @export var health_component: HealthComponent
+@export var fire_rate = 100.0
+@export var projectile_to_fire: PackedScene
+@export var projectile_spawn_location: Node2D
+
+var _last_fire_timestampt = 0.0
 
 signal projectile_fired
 signal died
 
 func _process(_delta: float) -> void:
 	if (Input.is_action_pressed("attack")):
-		# print(fire_controller)
-		fire_controller.try_fire()
+		try_fire()
 
 func _physics_process(_delta: float) -> void:
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var xDirection := Input.get_axis("ui_left", "ui_right")
 	var yDirection := Input.get_axis("ui_up", "ui_down")
 	if xDirection:
@@ -29,12 +29,20 @@ func _physics_process(_delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 0, speed)
 
 	move_and_slide()
-
-func _on_fire_controller_projectile_fired(target: Node2D) -> void:
-	projectile_fired.emit(target)
 	
 func take_damage(damage: Damage):
 	health_component.take_damage(damage)
 
 func _on_health_component_died() -> void:
 	died.emit()
+
+func try_fire():
+	var current_timestamp = Time.get_ticks_msec()
+	if (current_timestamp > _last_fire_timestampt + fire_rate):
+		_last_fire_timestampt = current_timestamp
+		fire()
+
+func fire():
+	var projectile: Node2D = projectile_to_fire.instantiate()
+	projectile.global_position = projectile_spawn_location.global_position
+	projectile_fired.emit(projectile)
